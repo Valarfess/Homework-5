c36cf3c53dff142a1c6f1f46bd03c06c2aee9caa
valarfess <valarfess@gmail.com>
2021-02-21 19:24:15 +0300

    Домашка 5

--- src/academy/belhard/entity/Person.java
+++ src/academy/belhard/entity/Person.java
@@ -2,15 +2,18 @@ package academy.belhard.entity;
 
 public class Person {
     protected String firstName;
-    public String getFirstName(){ return firstName; }
     protected String lastName;
-    public String getLastName(){ return lastName; }
 
     public Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
-
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
     public String getFullName() {
         String res1 = firstName + " " + lastName;
         return res1;
--- src/academy/belhard/entity/User.java
+++ src/academy/belhard/entity/User.java
@@ -7,25 +7,26 @@ import java.util.Objects;
 public class User extends Person {
 
     private String email;
-    public String getEmail(){ return email; }
     private String password;
-    public String getPassword(){ return password; }
 
     public User(String firstName, String lastName,String email, String password) {
         super(firstName, lastName);
         this.email = email;
         this.password = password;
     }
-
+    public String getEmail(){
+        return email;
+    }
+    public String getPassword(){
+        return password;
+    }
     public String getFullInfo() {
         String res2 = "Имя: " + firstName + " " + lastName + "\n" + "E-mail: " + email;
         return res2;
     }
-
     public boolean isPasswordCorrect(String password) {
         return this.password.equals(password);
     }
-
     public int hashCode() {
         return Objects.hash(password);
     }
